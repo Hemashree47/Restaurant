9e6a1dd95453eef3d491190ca6751de54b18636d
Hemashree47 <hemashree1910@gmail.com>
2024-04-02 21:25:00 +0530

    first commit

---
+++ fooditem.js
@@ -0,0 +1,404 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Tawa Fish Fry',
+    category : 'starters',
+    rating : 4.3,
+    price: 200,
+    img: 'images/Starters/tawa-fry.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Chicken Kabab(100 g)',
+    category : 'starters',
+    rating : 4.3,
+    price: 100,
+    img: 'images/Starters/c-65.jpeg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Tandoori Chicken',
+    category : 'starters',
+    rating : 4.3,
+    price: 250,
+    img: 'images/Starters/ctandoori2.jpeg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Paneer Tikka',
+    category : 'starters',
+    rating : 4.3,
+    price: 150,
+    img: 'images/Starters/paneer-tikka2.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Chicken Tikka',
+    category : 'starters',
+    rating : 4.3,
+    price: 180,
+    img: 'images/Starters/c-tikka2.jpeg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Chicken Lollipop',
+    category : 'starters',
+    rating : 4.3,
+    price: 200,
+    img: 'images/Starters/c-lollipop2.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Veg Kabab',
+    category : 'starters',
+    rating : 4.3,
+    price: 120,
+    img: 'images/Starters/veg-kabab.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Paneer Cutlet',
+    category : 'starters',
+    rating : 4.3,
+    price: 25,
+    img: 'images/Starters/cutlet.jpeg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Chicken Cutlet',
+    category : 'starters',
+    rating : 4.3,
+    price: 25,
+    img: 'images/Starters/c-cutlet.jpeg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Veg Meals ',
+    category : 'maincourse',
+    rating : 4.3,
+    price: 180,
+    img: 'images/maincourse/veg-meal2.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Non-veg Meals',
+    category : 'maincourse',
+    rating : 4.3,
+    price: 230,
+    img: 'images/maincourse/nonveg-meals.png',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Chicken Briyani',
+    category : 'maincourse',
+    rating : 4.3,
+    price: 200,
+    img: 'images/maincourse/c-briyani.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Mutton Briyani',
+    category : 'maincourse',
+    rating : 4.3,
+    price: 300,
+    img: 'images/maincourse/m-briyani.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Fish Briyani',
+    category : 'maincourse',
+    rating : 4.3,
+    price: 200,
+    img: 'images/maincourse/f-briyani.jpg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Veg Pulao',
+    category : 'maincourse',
+    rating : 4.3,
+    price: 150,
+    img: 'images/maincourse/veg-pulao.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Pani Puri',
+    category : 'chaats',
+    rating : 4.3,
+    price: 40,
+    img: 'images/chaats/panipoori.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'samosa',
+    category : 'chaats',
+    rating : 4.3,
+    price: 20,
+    img: 'images/chaats/samosa.jpeg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Mushroom Masala',
+    category : 'chaats',
+    rating : 4.3,
+    price: 45,
+    img: 'images/chaats/kaalan.jpeg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Bajji',
+    category : 'chaats',
+    rating : 4.3,
+    price: 12,
+    img: 'images/chaats/bajji.jpeg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Burger',
+    category : 'chaats',
+    rating : 4.3,
+    price: 150,
+    img: 'images/chaats/burger.jpg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Pizza',
+    category : 'chaats',
+    rating : 4.3,
+    price: 200,
+    img: 'images/chaats/pizza.jpeg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Masala Puri',
+    age: '32',
+    category : 'chaats',
+    rating : 4.3,
+    price: 45,
+    img: 'images/chaats/masala.jpeg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Icecream Varieties',
+    category : 'dessert',
+    rating : 4.3,
+    price: 50,
+    img: 'images/dessert/icecream.jpeg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Fresh Juices',
+    category : 'dessert',
+    rating : 4.3,
+    price: 60,
+    img: 'images/dessert/juices.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Kesari',
+    category : 'dessert',
+    rating : 4.3,
+    price: 30,
+    img: 'images/dessert/kesari2.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Gulab Jamun',
+    category : 'dessert',
+    rating : 4.3,
+    price: 10,
+    img: 'images/dessert/gj.jpeg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Cakes',
+    category : 'dessert',
+    rating : 4.3,
+    price: 45,
+    img: 'images/dessert/cake2.jpeg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Tender Payasam',
+    category : 'dessert',
+    rating : 4.3,
+    price: 50,
+    img: 'images/dessert/payasam.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Kulfi',
+    category : 'dessert',
+    rating : 4.3,
+    price: 40,
+    img: 'images/dessert/kulfi.jpeg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Chicken Rice',
+    category : 'fastfood',
+    rating : 4.3,
+    price: 150,
+    img: 'images/chinese/c-rice2.jpg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Chicken Manchurian',
+    category : 'fastfood',
+    rating : 4.3,
+    price: 150,
+    img: 'images/chinese/ChickenManchurian.jpg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Momos(5 pcs)',
+    category : 'fastfood',
+    rating : 4.3,
+    price: 100,
+    img: 'images/chinese/momos.jpg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Noodles',
+    category : 'fastfood',
+    rating : 4.3,
+    price: 150,
+    img: 'images/chinese/c-noodles.jpeg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Egg Kothu Parottaa',
+    category : 'fastfood',
+    rating : 4.3,
+    price: 150,
+    img: 'images/chinese/kothu.jpg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Szechuan Chicken',
+    category : 'fastfood',
+    rating : 4.3,
+    price: 200,
+    img: 'images/chinese/szechuan-chicken.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Veg Fried Rice',
+    category : 'fastfood',
+    rating : 4.3,
+    price: 120,
+    img: 'images/chinese/veg-fried-rice.jpg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Butter Masala Dosa',
+    category : 'tiffin',
+    rating : 4.3,
+    price: 100,
+    img: 'images/south indian/Butter-Masala-Dosa.png',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Idli(2)',
+    category : 'tiffin',
+    rating : 4.3,
+    price: 50,
+    img: 'images/south indian/idli-with-rice-flour.jpg',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Masala Dosa',
+    category : 'tiffin',
+    rating : 4.3,
+    price: 80,
+    img: 'images/south indian/masala-dosa.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Poori',
+    category : 'tiffin',
+    rating : 4.3,
+    price: 60,
+    img: 'images/south indian/poori.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Pongal',
+    category : 'tiffin',
+    rating : 4.3,
+    price: 50,
+    img: 'images/south indian/pongal.JPG',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Plain Dosa',
+    category : 'tiffin',
+    rating : 4.3,
+    price: 60,
+    img: 'images/south indian/plain-dosa.jpeg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Rava upma',
+    category : 'tiffin',
+    rating : 4.3,
+    price: 40,
+    img: 'images/south indian/upma.jpeg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Parotta(2 pcs)',
+    category : 'tiffin',
+    rating : 4.3,
+    price: 50,
+    img: 'images/south indian/parotta.jpeg',
+    quantity: 1
+},
+]
+
+export {foodItem};
